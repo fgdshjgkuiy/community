43f93ac46ab8b935c3302431e8beda8adda3b468
zhengys <1103655341@qq.com>
2023-03-11 13:50:45 +0800

    20220311swag

--- src/main/java/com/nowcoder/community/config/SwaggerConfig.java
+++ src/main/java/com/nowcoder/community/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
-//package com.nowcoder.community.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//dier
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//}
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
